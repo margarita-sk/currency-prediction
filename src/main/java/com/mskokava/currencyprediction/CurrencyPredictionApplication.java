4a09d5714cb14f98cb0ab3c5dd86f875e77102d6
Marharyta Skokava <marharyta.skokava@sap.com>
2022-07-24 20:59:27 +0200

    add properties

---
+++ src/main/java/com/mskokava/currencyprediction/CurrencyPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.mskokava.currencyprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyPredictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyPredictionApplication.class, args);
+    }
+
+}
